ae5b4ff68e56502a9271e917d175621e2e5c2ebf
C <armandminassian1@gmail.com>
2025-09-02 22:03:38 +0000

    bad permission, diff method

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    const url = new URL(details.url);
-
-    // Replace "m.wikipedia.org" with "wikipedia.org"
-    if (url.hostname.endsWith(".m.wikipedia.org")) {
-      const newUrl = url.href.replace(".m.wikipedia.org", ".wikipedia.org");
-      return { redirectUrl: newUrl };
-    }
-  },
-  { urls: ["*://*.m.wikipedia.org/*"] },
-  ["blocking"]
-);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: [1],
+    addRules: [
+      {
+        id: 1,
+        priority: 1,
+        action: {
+          type: "redirect",
+          redirect: { regexSubstitution: "https://\\1.wikipedia.org/\\2" }
+        },
+        condition: {
+          regexFilter: "^https://([a-z]+)\\.m\\.wikipedia\\.org/(.*)",
+          resourceTypes: ["main_frame"]
+        }
+      }
+    ]
+  });
+});
